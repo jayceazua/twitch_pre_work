2194644c724c9efb653f8e101de997d6e576c339
Jayce Azua <jayce.azua@gmail.com>
2020-04-05 15:11:04 -0400

    [wip] finish updating feature and delete feature on the edit page

--- edit.js
+++ edit.js
@@ -1,2 +1,32 @@
 const todoId = location.hash.substring(1)
-const todosJSON = localStorage.getItem('todos')
+
+// get saved todos -> get the premade function
+const todos = getSavedTodos()
+
+const todo = todos.find((todo) => {
+  return todo.id = todoId
+})
+
+// if note was not able to found redirect them to home page
+if (todo === undefined) {
+  location.assign('index.html')
+}
+
+// update todo
+document.querySelector('#update-todo').addEventListener('submit', (e) => {
+  e.preventDefault()
+  // update the todo object
+  todo.text = e.target.elements.text.value
+  // save the todo and redirect to the home page
+  saveTodos(todos)
+  location.assign('index.html')
+})
+
+// delete todo
+document.querySelector('#delete-todo').addEventListener('click', () => {
+  // delete todo
+  deleteTodo(todo.id)
+  saveTodos(todos)
+  // redirect to home page after button is deleted
+  location.assign('index.html')
+})
--- todos-functions.js
+++ todos-functions.js
@@ -100,3 +100,4 @@ const renderTodos = (todos, filters) => {
   })
 
 }
+
