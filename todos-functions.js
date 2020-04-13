480e5524480d40dabf5fb4793a922b207f337d9a
Jayce Azua <jayce.azua@gmail.com>
2020-04-13 10:07:47 -0400

    [Add] handle parsing errors

--- todos-functions.js
+++ todos-functions.js
@@ -1,7 +1,13 @@
 // read existing todos from localStorage
 const getSavedTodos = () => {
   const todosJSON = localStorage.getItem('todos')
-  return todosJSON ? JSON.parse(todosJSON) : []
+
+  try {
+    return todosJSON ? JSON.parse(todosJSON) : []
+  } catch (e) {
+    return []
+  }
+
 }
 
 // Save todo
